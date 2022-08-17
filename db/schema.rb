f1200e520fd4f93ea3816748ea9e3b9308569349
rua maruoka <rua.sun218@gmail.com>
2022-08-17 10:45:28 +0900

    petsテーブルに、user_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_070815) do
+ActiveRecord::Schema.define(version: 2022_08_16_152505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_08_16_070815) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_08_16_070815) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pets", "users"
 end
